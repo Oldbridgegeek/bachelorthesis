f4c7000acf594b022ccad798b6dc393e44edfe34
Enes Witwit <witwitenes@gmail.com>
2017-03-24 18:57:15 +0100

    Fixxed Penrose.

--- Matlab/includes/moore_penrose_prop.m
+++ Matlab/includes/moore_penrose_prop.m
@@ -6,21 +6,21 @@
 % For more Information check: de.wikipedia.org/wiki/Pseudoinverse#Die_Moore-Penrose-Inverse
 
 function fulfilled = moore_penrose_prop(A,A_moore)
-   if((A*A_moore*A) <= (A+0.0000000001))
-     if((A*A_moore*A) >= (A-0.0000000001))
-        if((A_moore*A*A_moore <= A_moore+0.0000000001))
-            if(A_moore*A*A_moore >= A_moore-0.0000000001)
-              if( transpose(A*A_moore) <= A*A_moore+0.0000000001 )
-                  if( transpose(A*A_moore) >= A*A_moore-0.0000000001 )
-                    if( transpose(A_moore*A) <= A_moore*A+0.0000000001)
-                      if( transpose(A_moore*A)>= A_moore*A-0.0000000001)
-                        fulfilled=1;
-                      else 
-                        fulfilled=0;
-                      end
-                    else
-                     fulfilled=0;
-                 end
+  if((A*A_moore*A) <= (A+0.0000000001))
+    if((A*A_moore*A) >= (A-0.0000000001))
+      if((A_moore*A*A_moore <= A_moore+0.0000000001))
+        if(A_moore*A*A_moore >= A_moore-0.0000000001)
+          if( transpose(A*A_moore) <= A*A_moore+0.0000000001 )
+           if( transpose(A*A_moore) >= A*A_moore-0.0000000001 )
+             if( transpose(A_moore*A) <= A_moore*A+0.0000000001)
+              if( transpose(A_moore*A)>= A_moore*A-0.0000000001)
+                fulfilled=1;
+              else 
+                fulfilled=0;
+              end
+            else
+              fulfilled=0;
+            end
                   else 
                       fulfilled=0;
                   end
