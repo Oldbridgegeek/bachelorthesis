2e440812d7515dfcc8dbddfe44123237e2e2dff9
Enes Witwit <witwitenes@gmail.com>
2017-05-21 14:45:36 +0200

    Refactoring.

--- Matlab/includes/tensorstructure/N.m
+++ Matlab/includes/tensorstructure/N.m
@@ -1,8 +1,4 @@
 function N = N(n)    
-    % Include necessary libraries and include files
-    addpath(genpath([pwd '/includes']));
-    addpath(genpath([pwd '/lib']));
-    
     % Compute quadrature nodes and weights
     [nodes,weights] = int_gauss_weights(n+1,0,1);
     for i=1:n+1
